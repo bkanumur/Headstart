4f420f4ddec43b2827fb41b30d122dbd13e5243b
Bhanu <145064788+bkanumur@users.noreply.github.com>
2024-07-25 20:42:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-<head>
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+});
